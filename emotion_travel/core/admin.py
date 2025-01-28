531872df7821a7f4d63c5c2c3e5879436645b46e
AdhunAsh <adhunav147@gmail.com>
2025-01-29 00:39:10 +0530

    [FEAT] db setup

--- emotion_travel/core/admin.py
+++ emotion_travel/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from core.models import Place , Image , Category , Emotion
 # Register your models here.
+
+admin.site.register(Place)
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Emotion)

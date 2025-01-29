0f29bba49639dd15fe030aba088c0b3661302ede
AdhunAsh <adhunav147@gmail.com>
2025-01-29 19:51:08 +0530

    [FEAT] new db model

--- emotion_travel/core/models.py
+++ emotion_travel/core/models.py
@@ -7,17 +7,18 @@ class Place(models.Model) :
     image = models.ImageField(upload_to='images/' , default=None)
     latitude = models.FloatField()
     longitude = models.FloatField()
-
-class Image(models.Model) :
-    images = models.ManyToManyField(Place , related_name= 'images')
        
 class Category(models.Model) :
     Name = models.CharField(max_length=25)
     image = models.ImageField(upload_to="images/" , default = None)
     places =models.ManyToManyField(Place , related_name= 'categories')
     
+class Image(models.Model) :
+    images = models.ManyToManyField(Place , related_name= 'images')
+
 class Emotion(models.Model) :
     emotion = models.CharField(max_length=30)
     message = models.CharField(max_length=255)
     categories = models.ManyToManyField(Category , related_name= 'emotions')
-    
+
+   
